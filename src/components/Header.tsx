5568cbe606d24a887c042149de33fd0dcc76af46
KeyserGuillaume <guillaumeby.dekeyser@gmail.com>
2024-09-24 23:58:19 +0200

    work on styling, add a little content on home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ function Header({onLogout}: Props) {
   // there is a useless div with flex-grow and transparent text
   // it is a hack to keep the page title centered
   return (
-    <header className="bg-orange-100 p-2 flex flex-row">
+    <header className="bg-orange-100 p-3 flex flex-row w-full">
       <div className="grow text-transparent select-none">Logout</div>
       <h1>One Post Is Enough</h1>
-      <div className="grow flex flex-row justify-end">
+      <div className="grow flex flex-row justify-end pr-[10px]">
         <button onClick={onLogout}>Logout</button>
       </div>
     </header>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -91,18 +91,45 @@ const HomePage = ({onLogout}: Props) => {
   };
 
   return (
-    <div>
+    <div className="w-full flex flex-col content-center flex-wrap">
       <Header onLogout={onLogout} />
-      <div className="w-dvw">
-        {imageUrl && (
-          <img
-            className="max-w-full"
-            src={imageUrl}
-            alt="This is the image selected among everything that was posted today"
-          />
-        )}
-        <h2>Add Image:</h2>
-        <input type="file" onChange={handleChange} accept="image/jpg" />
+      <div className="max-w-5xl flex flex-col flex-wrap mx-auto mt-10">
+        <p className="max-w-prose mx-auto">
+          Please find below the post for today, as well as the message of
+          introduction that the author attached to their post submission :
+        </p>
+        <hr className="my-4"></hr>
+        <div className="p-2 border-2 rounded-lg border-orange-100 mx-auto">
+          <blockquote className="max-w-prose">No message yet</blockquote>
+        </div>
+        <div>
+          {imageUrl && (
+            <img
+              className="mt-3 max-w-full lg:max-w-[1024px] "
+              src={imageUrl}
+              alt="This is the image selected among everything that was posted today"
+            />
+          )}
+        </div>
+        <hr className="my-4"></hr>
+        <p className="max-w-prose mx-auto">
+          You may submit a post if you wish. At the end of the day, one post
+          will be randomly selected among all posts submitted today, and this
+          will be the cover post for tomorrow. All other non-chosen submitted
+          posts will be permanently deleted. This process preserves the precious
+          time of all users of One Post Is Enough.
+        </p>
+        <p className="max-w-prose mx-auto">
+          Uploaded posts should meet the following rules : no violence or
+          nudity.
+        </p>
+        <h2 className="mx-auto mt-4">Add Image:</h2>
+        <input
+          className="mx-auto mt-2"
+          type="file"
+          onChange={handleChange}
+          accept="image/jpg"
+        />
         {file && <img src={file} />}
       </div>
     </div>
