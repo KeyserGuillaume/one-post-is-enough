4e2f852376abff271d3c998f7c164ba0573b7772
KeyserGuillaume <guillaumeby.dekeyser@gmail.com>
2024-09-28 09:57:34 +0200

    extract user post into another component, and add abort signal

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,121 +1,15 @@
-import {ChangeEvent, useEffect, useState} from 'react';
-import config from '../aws-config.json';
-import {getToken} from '../libs/auth-service';
-import {isFileJpg} from '../libs/utils';
+import {useState} from 'react';
 import Header from './Header';
 import SettingsPage from './SettingsPage';
 import {useTheOnePost} from '../hooks/useTheOnePost';
+import UserPostEditor from './UserPostEditor';
 
 type Props = {onLogout: () => void};
 
-const uploadThePost = async (file: File): Promise<boolean> => {
-  const token = await getToken();
-  if (token === null) {
-    return false;
-  }
-  const headers = new Headers();
-  headers.append('Authorization', token);
-
-  const fileIsCorrectFormat = await isFileJpg(file);
-
-  if (!fileIsCorrectFormat) {
-    window.alert('File is not jpg');
-    return false;
-  }
-
-  if (file.size > 1e6) {
-    window.alert('File is greater than 1 Mb');
-    return false;
-  }
-
-  const response = await fetch(config.apiEndpointUrl, {
-    method: 'POST',
-    headers,
-    body: file,
-  });
-  if (response.status === 200) {
-    return true;
-  } else {
-    try {
-      const responseMessage = await response.json();
-      console.log(responseMessage);
-    } catch {}
-  }
-  return false;
-};
-
-const fetchUserPost = async (): Promise<string | null> => {
-  const token = await getToken();
-  if (token === null) {
-    return null;
-  }
-  const headers = new Headers();
-  headers.append('Authorization', token);
-  headers.append('Accept', 'image/jpg');
-  const response = await fetch(config.apiEndpointUrl + '/user-post', {
-    headers,
-  });
-  if (response.status === 200) {
-    const responseText = await response.text();
-    const base64Decoded = await fetch(`data:image/jpeg;base64,${responseText}`);
-    const blob2 = await base64Decoded.blob();
-    const url = URL.createObjectURL(blob2);
-    return url;
-  } else {
-    return null;
-  }
-};
-
-const deleteUserPost = async () => {
-  const token = await getToken();
-  if (token === null) {
-    return false;
-  }
-  const headers = new Headers();
-  headers.append('Authorization', token);
-  const response = await fetch(config.apiEndpointUrl, {
-    method: 'DELETE',
-    headers,
-  });
-  if (response.status === 200) {
-    console.log('User post was deleted');
-  }
-};
-
 const HomePage = ({onLogout}: Props) => {
   const [displaySettings, setDisplaySettings] = useState(false);
-  const [uploadedPost, setUploadedPost] = useState<null | string>(null);
 
   const imageUrl = useTheOnePost();
-
-  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files === null) {
-      return;
-    }
-    const success = await uploadThePost(e.target.files[0]);
-    if (success) {
-      setUploadedPost(URL.createObjectURL(e.target.files[0].slice()));
-    }
-  };
-
-  useEffect(() => {
-    let ignore = false;
-    async function startFetching() {
-      const url = await fetchUserPost();
-      if (!ignore) {
-        setUploadedPost(url);
-      }
-    }
-    startFetching();
-    return () => {
-      ignore = true;
-    };
-  }, []);
-
-  const handleDeletePost = () => {
-    setUploadedPost(null);
-    deleteUserPost();
-  };
 
   const homePage = (
     <div className="w-full flex flex-col content-center flex-wrap">
@@ -142,40 +36,7 @@ const HomePage = ({onLogout}: Props) => {
           )}
         </div>
         <hr className="my-4"></hr>
-        <p className="max-w-prose mx-auto">
-          You may submit a post if you wish. At the end of the day, one post
-          will be randomly selected among all posts submitted today, and this
-          will be the cover post for tomorrow. All other non-chosen submitted
-          posts will be permanently deleted. This process preserves the precious
-          time of all users of One Post Is Enough.
-        </p>
-        <p className="max-w-prose mx-auto mt-2">
-          Uploaded posts should meet the following rules : no violence or
-          nudity.
-        </p>
-        {uploadedPost === null && (
-          <>
-            <h2 className="mx-auto mt-4">Add Image:</h2>
-            <input
-              className="mx-auto mt-2"
-              type="file"
-              onChange={handleChange}
-              accept="image/jpg"
-            />
-          </>
-        )}
-        {uploadedPost && (
-          <>
-            <img
-              className="mt-3 max-w-full lg:max-w-[1024px] mx-auto"
-              src={uploadedPost}
-              alt="This is the post that you've uploaded today"
-            />
-            <button className="mx-auto" onClick={handleDeletePost}>
-              Delete post
-            </button>
-          </>
-        )}
+        <UserPostEditor />
       </div>
     </div>
   );
---
+++ src/components/UserPostEditor.tsx
@@ -0,0 +1,158 @@
+import {ChangeEvent, useEffect, useRef, useState} from 'react';
+import {getToken} from '../libs/auth-service';
+import config from '../aws-config.json';
+import {isFileJpg} from '../libs/utils';
+
+const uploadThePost = async (file: File): Promise<boolean> => {
+  const token = await getToken();
+  if (token === null) {
+    return false;
+  }
+  const headers = new Headers();
+  headers.append('Authorization', token);
+
+  const fileIsCorrectFormat = await isFileJpg(file);
+
+  if (!fileIsCorrectFormat) {
+    window.alert('File is not jpg');
+    return false;
+  }
+
+  if (file.size > 1e6) {
+    window.alert('File is greater than 1 Mb');
+    return false;
+  }
+
+  const response = await fetch(config.apiEndpointUrl, {
+    method: 'POST',
+    headers,
+    body: file,
+  });
+  if (response.status === 200) {
+    return true;
+  } else {
+    try {
+      const responseMessage = await response.json();
+      console.log(responseMessage);
+    } catch {}
+  }
+  return false;
+};
+
+const fetchUserPost = async (signal: AbortSignal): Promise<string | null> => {
+  const token = await getToken();
+  if (token === null) {
+    return null;
+  }
+  const headers = new Headers();
+  headers.append('Authorization', token);
+  headers.append('Accept', 'image/jpg');
+  const response = await fetch(config.apiEndpointUrl + '/user-post', {
+    headers,
+    signal,
+  });
+  if (response.status === 200) {
+    const responseText = await response.text();
+    const base64Decoded = await fetch(`data:image/jpeg;base64,${responseText}`);
+    const blob2 = await base64Decoded.blob();
+    const url = URL.createObjectURL(blob2);
+    return url;
+  } else {
+    return null;
+  }
+};
+
+const deleteUserPost = async () => {
+  const token = await getToken();
+  if (token === null) {
+    return false;
+  }
+  const headers = new Headers();
+  headers.append('Authorization', token);
+  const response = await fetch(config.apiEndpointUrl, {
+    method: 'DELETE',
+    headers,
+  });
+  if (response.status === 200) {
+    console.log('User post was deleted');
+  }
+};
+
+const UserPostEditor = () => {
+  const [uploadedPost, setUploadedPost] = useState<null | string>(null);
+  const abortControllerRef = useRef<AbortController>(new AbortController());
+
+  const handleUpload = async (e: ChangeEvent<HTMLInputElement>) => {
+    abortControllerRef.current.abort();
+    if (e.target.files === null) {
+      return;
+    }
+    const success = await uploadThePost(e.target.files[0]);
+    if (success) {
+      setUploadedPost(URL.createObjectURL(e.target.files[0].slice()));
+    }
+  };
+
+  useEffect(() => {
+    let ignore = false;
+    abortControllerRef.current = new AbortController();
+    async function startFetching() {
+      try {
+        const url = await fetchUserPost(abortControllerRef.current.signal);
+        if (!ignore) {
+          setUploadedPost(url);
+        }
+      } catch {}
+    }
+    startFetching();
+    return () => {
+      abortControllerRef.current.abort();
+      ignore = true;
+    };
+  }, []);
+
+  const handleDeletePost = () => {
+    setUploadedPost(null);
+    deleteUserPost();
+  };
+
+  return (
+    <>
+      <p className="max-w-prose mx-auto">
+        You may submit a post if you wish. At the end of the day, one post will
+        be randomly selected among all posts submitted today, and this will be
+        the cover post for tomorrow. All other non-chosen submitted posts will
+        be permanently deleted. This process preserves the precious time of all
+        users of One Post Is Enough.
+      </p>
+      <p className="max-w-prose mx-auto mt-2">
+        Uploaded posts should meet the following rules : no violence or nudity.
+      </p>
+      {uploadedPost === null && (
+        <>
+          <h2 className="mx-auto mt-4">Add Image:</h2>
+          <input
+            className="mx-auto mt-2"
+            type="file"
+            onChange={handleUpload}
+            accept="image/jpg"
+          />
+        </>
+      )}
+      {uploadedPost && (
+        <>
+          <img
+            className="mt-3 max-w-full lg:max-w-[1024px] mx-auto"
+            src={uploadedPost}
+            alt="This is the post that you've uploaded today"
+          />
+          <button className="mx-auto" onClick={handleDeletePost}>
+            Delete post
+          </button>
+        </>
+      )}
+    </>
+  );
+};
+
+export default UserPostEditor;
